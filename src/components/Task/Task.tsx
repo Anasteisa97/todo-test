53c310942ae458baecf5262f421e72638e45e563
Anasteisa97 <nas.lazarewa@yandex.ru>
2023-09-05 23:55:07 +0300

    add ellipsis for title and description

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -8,28 +8,31 @@ const Task = ({id, title, completed}) => {
 
   return (
     <article className="task">
-      <div className="task-inner">
-        <div className="task-title">
-          <input type="checkbox" id={`task${id}`} checked={checked} onChange={() => setChecked(!checked)}/>
-          <label htmlFor={`task${id}`} className="fw-bold">{title}</label>
+      <label htmlFor={`task${id}`}>
+        <div className="task-inner">
+          <div className="task-top">
+            <input className="task__checkbox" type="checkbox" id={`task${id}`} checked={checked} onChange={() => setChecked(!checked)}/>
+            <span className="task__title fw-bold">{title}</span>
+          </div>
+          <div>
+            <div className="task-dates">
+              <span className="date fsz-13">Oct 12, 01:00 PM</span>
+              <span className="date fsz-13">Oct 01, 02:00 PM</span>
+            </div>
+            <p className="task__descr fsz-13">Task description with long longlong long long te</p>
+          </div>
+          <footer className="task-bot">
+            <div className="task-tags">
+              <span className="task-tags__item task-tags__item_violet">Entity title</span>
+              <span className="task-tags__item task-tags__item_gray">Front-end</span>
+            </div>
+            <div className="task-avatar">
+              <img src={Avatar} alt="avatar"/>
+            </div>
+          </footer>
         </div>
-        <div>
-          <div className="task-dates">
-            <span className="date fsz-13">Oct 12, 01:00 PM</span>
-            <span className="date fsz-13">Oct 01, 02:00 PM</span>
-          </div>
-          <p className="task-descr fsz-13">Task description with long long long te</p>
-        </div>
-        <footer className="task-bot">
-          <div className="task-tags">
-            <span className="task-tags__item task-tags__item_violet">Entity title</span>
-            <span className="task-tags__item task-tags__item_gray">Front-end</span>
-          </div>
-          <div className="task-avatar">
-            <img src={Avatar} alt="avatar"/>
-          </div>
-        </footer>
-      </div>
+      </label>
+
     </article>
   )
 }
